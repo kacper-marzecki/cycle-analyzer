9b7ec9b38183b73c46a8643132013cf67b5adda8
Kacper Marzecki <kacper-marzecki@github.noreply.com>
2020-05-14 09:47:55 +0200

    added frontend production files to repo

---
+++ front/build/precache-manifest.b0b47ca61735b2a47304f3ad336b22e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc357bd4a28fca89ca12c7af42cc805a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c4a51a504dc7cc58c7a",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "5b4123865227ac0daaa6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c4a51a504dc7cc58c7a",
+    "url": "/static/js/2.0b2407d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b2407d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4123865227ac0daaa6",
+    "url": "/static/js/main.f713c3ae.chunk.js"
+  },
+  {
+    "revision": "92273b49d3690110aa39",
+    "url": "/static/js/runtime-main.584e3335.js"
+  }
+]);
